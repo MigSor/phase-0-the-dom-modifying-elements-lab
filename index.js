6f56c8b40437c884750d435759acf1c02bb2b49d
Miguel Soriano <miguel.sor.88@gmail.com>
2023-07-16 12:00:22 +1000

    completed all the test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// test 1
+const main = document.querySelector("#main");
+
+main.remove();
+
+// test 2
+const newHeader = document.createElement("h1");
+
+// test 3
+newHeader.id = "victory";
+
+//test 4
+newHeader.textContent = "Miguel is the champion";
